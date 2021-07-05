c0caaf5fa38bf01b02389d724e4387960cedd458
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-07-05 15:36:14 -0300

    projeto criado

---
+++ src/main/kotlin/br/com/wagner/workshopmongo/WorkshopmongoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.wagner.workshopmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkshopmongoApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkshopmongoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/wagner/workshopmongo/WorkshopmongoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.wagner.workshopmongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
