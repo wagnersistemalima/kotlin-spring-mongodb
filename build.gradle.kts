a2f0f06e481df1ed58fe220fbf9f37ad85965fc0
wagner de lima braga silva <wagner.sistemalima@gmail.com>
2021-07-11 15:30:46 -0300

    image swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
